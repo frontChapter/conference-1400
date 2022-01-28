a5018ae3fa6e9566c6c54157ce1a5bb5bae40243
Amir Hossein Karimi <amir.hossein7464@gmail.com>
2022-01-28 20:50:57 +0330

    feat: center conteriner & add padding-x

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,10 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
+    container: {
+      center: true,
+      padding: '1rem',
+    },
     extend: {
       colors: {
         'primary': '#ff802e',
