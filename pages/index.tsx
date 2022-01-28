4c7a3d7e49b6e78c7039b33cf6e2d44ffa34a89e
Amir Hossein Karimi <amir.hossein7464@gmail.com>
2022-01-28 20:38:04 +0330

    feat: add header rings

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,6 +8,10 @@ export default function Home() {
         <title>همایش فرانت‌اند ۱۴۰۰ | فرانت چپتر</title>
       </Head>
       <TopNavbar />
+      <div className="relative overflow-hidden">
+        <div className="absolute top-0 left-0 h-80 w-80 -translate-x-1/2 -translate-y-1/2 rounded-full border-[40px] border-gray-200"></div>
+        <div className="absolute bottom-0 right-0 mr-16 h-80 w-80 translate-x-1/2 -translate-y-16 rounded-full border-[20px] border-gray-100"></div>
+      </div>
     </main>
   );
 }
