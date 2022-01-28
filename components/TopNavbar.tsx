d2fc8f86f6441fc7cc16cb606b2807945cd41b48
Amir hossein Karimi <amir.hossein7464@gmail.com>
2022-01-28 17:24:38 +0330

    feat: add HamburgetButton

--- components/TopNavbar.tsx
+++ components/TopNavbar.tsx
@@ -2,20 +2,20 @@ import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
 import logo from "../assets/images/logo.png";
+import { HamburgerButton } from "./HamburgerButton";
 import { TopNav } from "./TopNav";
 
 export const TopNavbar: React.FC<{}> = () => {
   let [navCollapsed, setNavCollapse] = useState<boolean>(false);
 
-  const toggleNav = () => {
-    setNavCollapse(!navCollapsed);
-  };
-
   return (
     <div className="fixed inset-x-0 top-0 bg-white">
       <div className="container mx-auto flex flex-row flex-wrap items-center justify-between px-4 py-3 lg:justify-start">
-        <div className="order-1 lg:hidden">
-          <button onClick={toggleNav}>#</button>
+        <div className="order-1 h-9 w-9 lg:hidden">
+          <HamburgerButton
+            collapsed={navCollapsed}
+            setNavCollapse={setNavCollapse}
+          />
         </div>
         <div className="order-2">
           <Link href="/">
@@ -35,13 +35,8 @@ export const TopNavbar: React.FC<{}> = () => {
         <div className="order-3 lg:order-4 lg:mr-auto">
           <button>ثبت‌نام</button>
         </div>
-        <div
-          className={
-            "collapse lg:collapsed order-3 w-full duration-300 lg:order-3 lg:mr-2 lg:w-auto" +
-            (navCollapsed ? " collapsed" : "")
-          }
-        >
-          <TopNav />
+        <div className="order-3 w-full lg:order-3 lg:mr-2 lg:w-auto">
+          <TopNav collapsed={navCollapsed} setNavCollapse={setNavCollapse} />
         </div>
       </div>
     </div>
