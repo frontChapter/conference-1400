5aa97065f8b23a97470b013d348fe312f27bdf3c
Amir Hossein Karimi <amir.hossein7464@gmail.com>
2022-01-28 20:37:25 +0330

    fix: update page padding-top

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import { TopNavbar } from "../components/TopNavbar";
 
 export default function Home() {
   return (
-    <main className="content min-h-screen bg-gray-50 pt-20">
+    <main className="content min-h-screen bg-gray-50 pt-[72px]">
       <Head>
         <title>همایش فرانت‌اند ۱۴۰۰ | فرانت چپتر</title>
       </Head>
