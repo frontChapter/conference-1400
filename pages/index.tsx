f7d38fe09af36eed26424c5256a7b4adfba83d1b
Saleh Shojaei <ordinarysaleh@pm.me>
2022-01-29 15:50:08 +0330

    speakers components;

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
 import Head from "next/head";
 import { Jumbotron } from "../components/Jumbotron";
+import Speakers from "../components/Speakers";
 import { TopNavbar } from "../components/TopNavbar";
+import TripSteps from "../components/TripSteps";
 
 export default function Home() {
   return (
@@ -9,11 +11,15 @@ export default function Home() {
         <title>همایش فرانت‌اند ۱۴۰۰ | فرانت چپتر</title>
       </Head>
       <TopNavbar />
-      <div className="relative overflow-hidden">
+      <div className="overflow-hidden relative">
         <div className="absolute top-0 left-0 h-80 w-80 -translate-x-1/2 -translate-y-1/2 rounded-full border-[40px] border-gray-200"></div>
         <div className="absolute bottom-0 right-0 mr-16 h-80 w-80 translate-x-1/2 -translate-y-16 rounded-full border-[20px] border-gray-100"></div>
         <Jumbotron />
       </div>
+      <div className="container px-6 mx-auto">
+        <Speakers />
+      </div>
+      <TripSteps />
     </main>
   );
 }
