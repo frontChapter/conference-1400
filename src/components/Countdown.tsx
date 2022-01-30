936285fc67d9fa362cfd8ea924797e43d140cfde
Amir Hossein Karimi <amir.hossein7464@gmail.com>
2022-01-31 00:30:38 +0330

    feat: add Countdown functionality

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -1,30 +1,19 @@
+import useSWR from "swr";
 import moment from "moment";
 import { digitsEnToFa } from "@persian-tools/persian-tools";
 import { useEffect, useState } from "react";
 
-const Countdown: React.FC<{}> = () => {
-  const diff = moment("2022-03-01 09:00:00").diff(moment());
-  const [remain, setRemain] = useState(diff);
-  const duration = moment.duration(remain);
+const fetcher = (url: string) => fetch(url).then((r) => r.text());
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setRemain((remain) => remain - 1000);
-    }, 1000);
-    return () => clearInterval(interval);
-  }, []);
+const Countdown: React.FC<{}> = () => {
+  const { data: remainingTime } = useSWR("/api/remain", fetcher);
+  const remain = remainingTime ? parseInt(remainingTime) : 0;
 
   return (
     <div className="container relative flex flex-col items-center justify-center py-6 md:flex-row md:py-12">
       <div className="text-2xl font-bold text-primary md:text-4xl">فقط</div>
       <div className="my-4 flex space-x-1 space-x-reverse sm:space-x-3 md:my-0 md:mx-8">
-        <SingleTime value={duration.seconds()} label="ثانیه" />
-        <Divider />
-        <SingleTime value={duration.minutes()} label="دقیقه" />
-        <Divider />
-        <SingleTime value={duration.hours()} label="ساعت" />
-        <Divider />
-        <SingleTime value={duration.days()} label="روز" />
+        <CountdownTimer remain={remain} />
       </div>
       <div className="flex flex-col text-center md:text-right">
         <div className="text-2xl leading-9 text-gray-400">تا شروع اولین</div>
@@ -38,6 +27,38 @@ const Countdown: React.FC<{}> = () => {
   );
 };
 
+const CountdownTimer: React.FC<{ remain: number }> = ({
+  remain: rawRemain,
+}) => {
+  const [remain, setRemain] = useState(0);
+  const duration = moment.duration(remain);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (!remain) return;
+      setRemain((remain) => remain - 1000);
+    }, 1000);
+    return () => clearInterval(interval);
+  });
+
+  useEffect(() => {
+    if (!rawRemain) return;
+    setRemain(rawRemain);
+  }, [rawRemain]);
+
+  return (
+    <>
+      <SingleTime value={duration.seconds()} label="ثانیه" />
+      <Divider />
+      <SingleTime value={duration.minutes()} label="دقیقه" />
+      <Divider />
+      <SingleTime value={duration.hours()} label="ساعت" />
+      <Divider />
+      <SingleTime value={duration.days()} label="روز" />
+    </>
+  );
+};
+
 const SingleTime: React.FC<{ label: string; value: number }> = ({
   label,
   value,
@@ -46,7 +67,7 @@ const SingleTime: React.FC<{ label: string; value: number }> = ({
     <div>
       <div className="flex h-16 w-16 items-center justify-center rounded-2xl border-[3px] border-[#fbdede] bg-gradient-to-tl from-[#ff8d27] to-[#e54404]">
         <div className="text-2rem font-black leading-none text-white">
-          {digitsEnToFa(value)}
+          {value !== 0 ? digitsEnToFa(Math.max(0, value)) : "?"}
         </div>
       </div>
       <div className="mt-[6px] text-center text-lg font-medium text-gray-500">
