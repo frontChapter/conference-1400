8f88a1cb903f7d8c6c1942577eae6df3a573cb6f
Amir Hossein Karimi <amir.hossein7464@gmail.com>
2022-01-30 15:36:13 +0330

    feat: responsive Jumbotron

--- src/components/Jumbotron.tsx
+++ src/components/Jumbotron.tsx
@@ -6,9 +6,9 @@ import WaveSvg from "assets/images/jumbotron-wave.svg?inline";
 export const Jumbotron: React.FC<{}> = () => {
   return (
     <div className="container py-8">
-      <div className="relative overflow-hidden rounded-[75px] pt-20 pb-28">
+      <div className="relative overflow-hidden rounded-[75px] py-8 px-4 md:pt-20 md:pb-28">
         <Image
-          alt="Mountains"
+          alt="تصویر همایش"
           src={bg}
           layout="fill"
           objectFit="cover"
@@ -17,7 +17,7 @@ export const Jumbotron: React.FC<{}> = () => {
 
         <div className="relative flex flex-col items-center">
           <div className="flex items-center">
-            <figure className="ml-3 h-12 w-12">
+            <figure className="ml-3 h-6 w-6 md:h-12 md:w-12">
               <Image
                 src={logo}
                 priority
@@ -27,23 +27,27 @@ export const Jumbotron: React.FC<{}> = () => {
                 alt="فرانت چپتر"
               />
             </figure>
-            <span className="text-3xl font-medium text-white">فرانت چپتر</span>
+            <span className="text-xl font-medium text-white md:text-3xl">
+              فرانت چپتر
+            </span>
           </div>
 
-          <div className="mt-12">
-            <h1 className="font-heading text-[64px] text-white">
-              {`بزرگترین همایش `}
+          <div className="mt-6 md:mt-12">
+            <h1 className="text-center font-heading text-4xl text-white md:text-[64px] md:leading-tight">
+              {`بزرگ ترین `}
+              <br className="lg:hidden" />
+              {`همایش `}
               <span className="text-primary">فرانت‌اند</span>
               {` کشور`}
             </h1>
           </div>
 
           <div className="mt-4 flex items-center justify-center text-white">
-            <i className="ri-map-pin-2-line ri-xl ml-2" />
-            <div className="text-2xl">۱۰ اسفند ۱۴۰۰</div>
-            <div className="mx-4 text-2xl">-</div>
-            <i className="ri-calendar-line ri-xl ml-2" />
-            <div className="text-2xl">مازندران، بابلسر</div>
+            <i className="ri-map-pin-2-line ml-2 leading-none md:text-2xl" />
+            <div className="md:text-2xl">۱۰ اسفند ۱۴۰۰</div>
+            <div className="mx-2 md:mx-4 md:text-2xl">-</div>
+            <i className="ri-calendar-line ml-2 leading-none md:text-2xl" />
+            <div className="md:text-2xl">مازندران، بابلسر</div>
           </div>
 
           <div className="mt-12">
@@ -51,13 +55,11 @@ export const Jumbotron: React.FC<{}> = () => {
               href="https://evand.com/events/frontchapter-1400"
               target="_blank"
               rel="noreferrer"
-              className="btn-orange rounded-3xl px-7 py-1 text-2xl font-bold leading-10 shadow-md"
+              className="btn-orange rounded-3xl px-4 py-1 font-bold shadow-md md:px-7 md:text-2xl md:leading-10"
             >
               ثبت نام
             </a>
           </div>
-
-          {/* TODO: Add wave and arrow on bottom */}
         </div>
 
         <WaveSvg className="absolute bottom-0 left-1/2 hidden -translate-x-1/2 text-gray-50 md:block" />
