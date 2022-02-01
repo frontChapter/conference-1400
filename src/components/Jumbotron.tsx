2d0812411510e5143e87d58e4525ca92bc483029
Amir Hossein Karimi <amir.hossein7464@gmail.com>
2022-02-02 00:09:30 +0330

    refactor: format Jumbotron before modify

--- src/components/Jumbotron.tsx
+++ src/components/Jumbotron.tsx
@@ -7,17 +7,11 @@ const Jumbotron: React.FC<{}> = () => {
   return (
     <div className="container py-8">
       <div className="relative overflow-hidden rounded-[20px] py-8 px-4 sm:rounded-[75px] md:pt-20 md:pb-28">
-        <Image
-          alt="تصویر همایش"
-          src={bg}
-          layout="fill"
-          objectFit="cover"
-          quality={100}
-        />
+        <Image alt="تصویر همایش" src={bg} layout="fill" objectFit="cover" quality={100} />
 
-        <div className="flex relative flex-col items-center">
+        <div className="relative flex flex-col items-center">
           <div className="flex items-center">
-            <figure className="ml-3 w-6 h-6 md:h-12 md:w-12">
+            <figure className="ml-3 h-6 w-6 md:h-12 md:w-12">
               <Image
                 src={logo}
                 priority
@@ -27,9 +21,7 @@ const Jumbotron: React.FC<{}> = () => {
                 alt="فرانت چپتر"
               />
             </figure>
-            <span className="text-xl font-medium text-white md:text-3xl">
-              فرانت چپتر
-            </span>
+            <span className="text-xl font-medium text-white md:text-3xl">فرانت چپتر</span>
           </div>
 
           <div className="mt-6 md:mt-12">
@@ -42,11 +34,11 @@ const Jumbotron: React.FC<{}> = () => {
             </h1>
           </div>
 
-          <div className="flex justify-center items-center mt-4 text-white">
-            <i className="ml-2 leading-none ri-map-pin-2-line md:text-2xl" />
+          <div className="mt-4 flex items-center justify-center text-white">
+            <i className="ri-map-pin-2-line ml-2 leading-none md:text-2xl" />
             <div className="md:text-2xl">۱۰ اسفند ۱۴۰۰</div>
             <div className="mx-2 md:mx-4 md:text-2xl">-</div>
-            <i className="ml-2 leading-none ri-calendar-line md:text-2xl" />
+            <i className="ri-calendar-line ml-2 leading-none md:text-2xl" />
             <div className="md:text-2xl">مازندران، بابلسر</div>
           </div>
 
@@ -55,16 +47,16 @@ const Jumbotron: React.FC<{}> = () => {
               href="https://evand.com/events/frontchapter-1400"
               target="_blank"
               rel="noreferrer"
-              className="px-4 py-1 font-bold rounded-3xl shadow-md btn-orange md:px-7 md:text-2xl md:leading-10"
+              className="btn-orange rounded-3xl px-4 py-1 font-bold shadow-md md:px-7 md:text-2xl md:leading-10"
             >
               ثبت نام
             </a>
           </div>
         </div>
 
-        <WaveSvg className="hidden absolute bottom-0 left-1/2 text-gray-50 -translate-x-1/2 md:block" />
-        <div className="hidden absolute bottom-0 left-1/2 -translate-x-1/2 translate-y-1 md:block">
-          <i className="block text-4xl leading-none text-gray-800 animate-bounce ri-arrow-down-s-line" />
+        <WaveSvg className="absolute bottom-0 left-1/2 hidden -translate-x-1/2 text-gray-50 md:block" />
+        <div className="absolute bottom-0 left-1/2 hidden -translate-x-1/2 translate-y-1 md:block">
+          <i className="ri-arrow-down-s-line block animate-bounce text-4xl leading-none text-gray-800" />
         </div>
       </div>
     </div>
