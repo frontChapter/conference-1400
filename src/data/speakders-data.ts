0569bd353554ccd045d44fa75218b8b328361db0
Amir Hossein Karimi <amir.hossein7464@gmail.com>
2022-02-12 17:33:54 +0330

    feat: update speakers-data

--- src/data/speakders-data.ts
+++ src/data/speakders-data.ts
@@ -1,4 +1,8 @@
 // import miladMoafiAvatar from "assets/images/speakers/milad-moafi.png";
+import balehLogo from "assets/images/companies/baleh.png";
+import liaraLogo from "assets/images/companies/liara.png";
+import snappLogo from "assets/images/companies/snapp.png";
+import snappMarketLogo from "assets/images/companies/snapp-market.png";
 import sobhanAvatar from "assets/images/speakers/sobhan.jpg";
 import miladAzamiAvatar from "assets/images/speakers/milad-azami.png";
 import amirAliGholiAvatar from "assets/images/speakers/amirali-gholi.png";
@@ -7,46 +11,49 @@ import mohammadMahmoodiAvatar from "assets/images/speakers/mohammad_mahmoodi.jpg
 
 export const speakersList: speaker[] = [
   {
+    photo: amirAliGholiAvatar,
     name: "امیرعلی قلی",
     position: "توسعه دهنده‌ی جاوااسکریپت",
-    company: "لیارا",
-    photo: amirAliGholiAvatar,
-    brandClass: "text-[#303030]",
+    companyName: "لیارا",
+    companyLogo: liaraLogo,
   },
   {
+    photo: sobhanAvatar,
     name: "سبحان امین‌نژاد",
     position: "توسعه‌دهنده‌ی ارشد فرانت‌اند",
-    company: "اسنپ مارکت",
-    photo: sobhanAvatar,
-    brandClass: "text-[#3a52ff]",
+    companyName: "اسنپ مارکت",
+    companyLogo: snappMarketLogo,
   },
   {
     name: "میلاد اعظمی",
     position: "اینفولوئنسر فرانت‌اند",
-    company: "فریلنسر",
+    companyName: "فریلنسر",
     photo: miladAzamiAvatar,
     brandClass: "from-[#02abdf] to-[#01bd9c] bg-gradient-to-r bg-clip-text text-transparent",
+    hideCompanyPrefix: true,
   },
   {
     name: "محمدرضا اعلا",
     position: "مهندس فرانت‌اند",
-    company: "اسنپ",
+    companyName: "اسنپ",
     photo: mrAlaAvatar,
-    brandClass: "text-[#00d170]",
+    companyLogo: snappLogo,
   },
   {
     name: "محمود محمودی",
     position: "راهبر تیم فنی",
-    company: "پیام‌رسان بانکی بله",
+    companyName: "پیام‌رسان بانکی بله",
     photo: mohammadMahmoodiAvatar,
-    brandClass: "from-[#335280] to-[#4cebb4] bg-gradient-to-r bg-clip-text text-transparent",
+    companyLogo: balehLogo,
   },
 ];
 
 export interface speaker {
+  photo: StaticImageData;
   name: string;
   position: string;
-  company: string;
+  companyName?: string;
   brandClass?: string;
-  photo: StaticImageData;
+  companyLogo?: StaticImageData;
+  hideCompanyPrefix?: boolean;
 }
