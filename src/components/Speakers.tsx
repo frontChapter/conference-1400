25b784824a300766644473f32b740ff4bd37333c
Amir Hossein Karimi <amir.hossein7464@gmail.com>
2022-02-12 16:56:08 +0330

    refactor: re-order components in Speakers

--- src/components/Speakers.tsx
+++ src/components/Speakers.tsx
@@ -1,6 +1,25 @@
 import { speaker, speakersList } from "data/speakders-data";
 import Image from "next/image";
 
+const Speakers = () => {
+  return (
+    <section id="speakers" className="bg-white">
+      <div className="container py-9 md:py-12 lg:py-16">
+        <h3 className="mb-5 text-center text-2xl font-black leading-normal text-primary md:mb-7 md:text-28 lg:mb-9 lg:text-4xl">
+          ارائه دهندگان
+        </h3>
+        <div className="flex justify-center">
+          <div className="mx-auto flex flex-wrap justify-center gap-x-8 gap-y-8 md:gap-y-16 md:gap-x-16 lg:gap-x-6 xl:gap-x-8">
+            {speakersList.map((item, index) => (
+              <ListItem key={index} {...item} />
+            ))}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
 const ListItem = ({ photo, name, position, company, brandClass }: speaker) => (
   <div className={"flex w-44 flex-col items-center"}>
     <figure className="h-32 w-32">
@@ -26,24 +45,5 @@ const ListItem = ({ photo, name, position, company, brandClass }: speaker) => (
     </span>
   </div>
 );
-
-const Speakers = () => {
-  return (
-    <section id="speakers" className="bg-white">
-      <div className="container py-9 md:py-12 lg:py-16">
-        <h3 className="mb-5 text-center text-2xl font-black leading-normal text-primary md:mb-7 md:text-28 lg:mb-9 lg:text-4xl">
-          ارائه دهندگان
-        </h3>
-        <div className="flex justify-center">
-          <div className="mx-auto flex flex-wrap justify-center gap-x-8 gap-y-8 md:gap-y-16 md:gap-x-16 lg:gap-x-6 xl:gap-x-8">
-            {speakersList.map((item, index) => (
-              <ListItem key={index} {...item} />
-            ))}
-          </div>
-        </div>
-      </div>
-    </section>
-  );
-};
 
 export default Speakers;
