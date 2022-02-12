ccbd539110a7804dba898f60feb4809f610c4c94
Amir Hossein Karimi <amir.hossein7464@gmail.com>
2022-02-12 19:40:47 +0330

    feat: add social media to Speakers item

--- src/components/Speakers.tsx
+++ src/components/Speakers.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import { speaker, speakersList } from "data/speakders-data";
+import { links, socialMediaIcons, speaker, speakersList } from "data/speakders-data";
 
 const Speakers = () => {
   return (
@@ -9,8 +9,8 @@ const Speakers = () => {
       </h3>
       <div className="flex justify-center">
         <div className="mx-auto flex flex-wrap justify-center gap-x-8 gap-y-8 md:gap-y-16 md:gap-x-16 lg:gap-x-6 xl:gap-x-8">
-          {speakersList.map((item, index) => (
-            <ListItem key={index} {...item} />
+          {speakersList.map((speaker, index) => (
+            <ListItem key={index} {...speaker} />
           ))}
         </div>
       </div>
@@ -23,11 +23,12 @@ const ListItem = ({
   name,
   position,
   companyName,
-  companyLogo,
   brandClass,
+  companyLogo,
   hideCompanyPrefix,
+  links,
 }: speaker) => (
-  <div className="flex w-44 flex-col items-center space-y-4 rounded-3xl bg-white px-3 py-6">
+  <div className="flex w-48 flex-col items-center space-y-4 rounded-3xl bg-white px-3 py-6">
     <figure className="h-32 w-32">
       <Image
         src={photo}
@@ -47,7 +48,7 @@ const ListItem = ({
         )}
         {companyLogo ? (
           <figure className="flex">
-            <Image src={companyLogo} alt={companyName} />
+            <Image src={companyLogo} alt={companyName} title={companyName} />
           </figure>
         ) : (
           <span
@@ -60,7 +61,32 @@ const ListItem = ({
         )}
       </div>
     </div>
+    {links && <Links links={links} />}
   </div>
 );
+
+const Links: React.FC<{
+  links: links;
+}> = ({ links }) => {
+  return (
+    <div
+      className="flex items-center justify-center space-x-3 space-x-reverse"
+      onClick={(e) => e.stopPropagation()}
+    >
+      {Object.keys(links).map((key) => {
+        const link = links[key as keyof links];
+        const icon = socialMediaIcons[key as keyof typeof socialMediaIcons];
+
+        return (
+          <a key={key} href={link} target="_blank" rel="noreferrer">
+            <i
+              className={`${icon} transi block text-xl leading-none text-gray-400 transition-colors hover:text-gray-500`}
+            />
+          </a>
+        );
+      })}
+    </div>
+  );
+};
 
 export default Speakers;
