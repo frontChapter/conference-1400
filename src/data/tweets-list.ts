d5e49dee3c3e9803f02dbdb4b6b42b390f26fed5
Amir Hossein Karimi <amir.hossein7464@gmail.com>
2022-02-26 20:13:43 +0330

    feat: add tweets-list data

---
+++ src/data/tweets-list.ts
@@ -0,0 +1,9 @@
+export const tweetsList: string[] = [
+  // Samples
+  // "https://twitter.com/Peivast/status/1497589812286603270",
+  // "https://twitter.com/shanbemag1/status/1493897490483093508",
+  // "https://twitter.com/stup360/status/1497607070559965187",
+  // "https://twitter.com/alopeyk/status/1496869308428922884",
+  // "https://twitter.com/liara_cloud/status/1496442261969997824",
+  // "https://twitter.com/stup360/status/1497607070559965187",
+];
